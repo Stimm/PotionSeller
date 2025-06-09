42bae5be5daa677cbba55a74878d05cb119a0ca4
Stimm <timothyryan.dublin@gmail.com>
2025-06-09 09:38:19 +0100

    Added in basic Inventory model and test project

---
+++ PotionSeller/Models/Inventory.cs
@@ -0,0 +1,11 @@
+﻿namespace Inventory.Models
+{
+    public class Inventory
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Discription { get; set; }
+        public required string Type { get; set; }
+        public int Amount { get; set; }
+    }
+}
